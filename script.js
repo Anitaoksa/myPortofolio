8b4525eefbc55051eb2f327cd5db67e016c7f23b
Anitaoksa <anitaoksa@student.uns.ac.id>
2025-01-09 21:54:24 +0700

    Missing Images

---
+++ script.js
@@ -0,0 +1,18 @@
+function sendMail(event) {
+    event.preventDefault(); // Mencegah reload halaman
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_8q8rgre", "template_5wnw6yi", parms)
+        .then(function(response) {
+            console.log("SUCCESS!", response.status, response.text);
+            alert("Email Sent Successfully!");
+        })
+        .catch(function(error) {
+            console.error("FAILED...", error);
+            alert("Failed to send email. Error: " + JSON.stringify(error));
+        });
+}
