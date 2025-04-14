b16c1816f3eeb097700b4c25fc53a5f72db77aa4
Anitaoksa <anitaoksa@student.uns.ac.id>
2025-04-14 22:05:01 +0700

    Update index.html dan tambah konten portofolio

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,4 @@
+// Fungsi untuk memeriksa apakah form sudah lengkap
 function checkForm() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
@@ -13,3 +14,17 @@ function checkForm() {
         sendButton.disabled = true;   // Nonaktifkan tombol Send jika ada field yang kosong
     }
 }
+
+// Fungsi untuk mengubah warna background navigasi saat scroll
+function changeNavBackground() {
+    var navbar = document.querySelector(".navbar");
+    
+    console.log(document.body.scrollTop, document.documentElement.scrollTop); // Cek posisi scroll
+
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+}
+
